905432fd67197b17a49a69af29c237fea4dde068
narumon <narue@outlook.com>
2019-10-06 19:53:19 +0700

    edit Classify.dart

--- lib/page/ClassifyPage.dart
+++ lib/page/ClassifyPage.dart
@@ -8,13 +8,16 @@ class _ClassifyPageState extends State<ClassifyPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       appBar: AppBar(
-         title: Text('Classify'),
-         backgroundColor: Color(0xffF95959),
-       ),
-       body: Center(
-         child: Text('Classify-Page'),
-       ),
+      appBar: AppBar(
+        title: Text('Classify'),
+        backgroundColor: Color(0xffF95959),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Classify PPPPP'),
+          color: Color(0xffF95959),
+        )
+      ),
     );
   }
 }
